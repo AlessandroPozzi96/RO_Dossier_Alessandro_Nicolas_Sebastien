1ef1f4782ae60e5b645946a07d63153ab3de12ba
Gwynbleidd96 <33867850+Gwynbleidd96@users.noreply.github.com>
2017-11-25 10:58:20 +0100

    Initialisation des tableaux + tentative pour génération le nombre darrivé

--- RO_Dossier_Alessandro_Nicolas_Sébastien/Header.h
+++ RO_Dossier_Alessandro_Nicolas_Sébastien/Header.h
@@ -11,16 +11,35 @@
 #define TEMPSSIMULE 960
 #define CAPACITE_FILE_EXPRESS 8
 #define NBSTATION_EXPRESS 2
+#define NBCLIENTSNORMALE 1000
+#define LAMBDA 2.2
+#define COUT_CLIENT_EXPRESS 37.5
+#define COUT_CLIENT_NORMALE 25.5
+#define COUT_CLIENT_PRIORITAIRE	42.5
+#define COUT_1HEURE_EXPRESS 30
+#define COUT_1HEURE_NORMALE 35
+#define COUT_INOCCUPATION 18
+#define EXPRESS_VERS_NORMALE 5
+
+//Structures
+
+typedef struct clientNormale ClientNormale;
+struct clientNormale
+{
+	int DS;
+	bool estPrioritaire;
+};
 
 //Prototypes de fonctions
 
 int GénérerNbArriv(void);
+int rand_a_b(int a, int b);
 int GénérerDS(void);
-void initTabStation(int *tabNormale, int *tabExpress, int *tabFileNormale, int *tabFileExpress);
-void placerClientFile(int nbArrivé, int *fileExpress, int *fileNormale, int *DS, int *tabNormale, int *tabFileNormale, int *tabExpress, int *tabFileExpress);
+void initTableaux(int *tabNormale, int *tabExpress, ClientNormale *tabFileNormale, int *tabFileExpress);
+void placerClientFile(int nbArrivé, int *fileExpress, int *fileNormale, int *DS, int *tabNormale, ClientNormale *tabFileNormale, int *tabExpress, int *tabFileExpress);
 void trtStationExpress(int iStation, int *tabStation, int *fileExpress, int *tabFileExpress, int *nbMinInoccupation, int *minExpress);
-void trtStationNormale(int iStation, int *tabStation, int *fileNormale, int tabFileNormale, int *nbPrioritairesAbsolu, int *minNormale);
-void trtPrioritaire(int iStation, int *tabStation, int *tabFileNormale, int *nbPrioritairesAbsolu);
+void trtStationNormale(int iStation, int *tabStation, int *fileNormale, ClientNormale *tabFileNormale, int *nbPrioritairesAbsolu, int *minNormale);
+void trtPrioritaire(int iStation, int *tabStation, ClientNormale *tabFileNormale, int *nbPrioritairesAbsolu);
 void calcul(int nbPersCumNormale, int nbPersCumExpress, int nbPriorCumAbsolu, int minExpress, int minNormale, int minInoccupation, int expressNormaleTot, int *tabCout);
 int rechercheMin(int *tabCout);
 
--- RO_Dossier_Alessandro_Nicolas_Sébastien/Main.c
+++ RO_Dossier_Alessandro_Nicolas_Sébastien/Main.c
@@ -7,7 +7,7 @@ void main(void)
 	int tabExpress[NBSTATION_EXPRESS];
 	int tabFileExpress[CAPACITE_FILE_EXPRESS];
 	int tabNormale[SMAX];
-	int tabFileNormale[1000];
+	ClientNormale tabFileNormale[NBCLIENTSNORMALE]; //File en théorie illimitée...
 	int tabCout[SMAX];
 	int s = 3;
 	s = SMIN;
@@ -17,7 +17,7 @@ void main(void)
 		//Initialisations
 		fileNormal = fileExpress = nbMinInoccupation = expressNormaleTot = nbPrioritairesAbsolu = minExpress = minNormal = nbPersCumExpress = nbPersCumNormale = nbPriorCumAbsolu;
 		t = 1;
-		initTabStation(tabNormale, tabExpress, tabFileNormale, tabFileExpress);
+		initTableaux(tabNormale, tabExpress, tabFileNormale, tabFileExpress);
 
 		while (t <= SMAX)
 		{
@@ -40,4 +40,56 @@ void main(void)
 	sOptimal = rechercheMin(tabCout);
 
 	system("pause");
+}
+
+void initTableaux(int *tabNormale, int *tabExpress, ClientNormale *tabFileNormale, int *tabFileExpress)
+{
+	for (int iNorm = 0; iNorm < SMAX; iNorm++)
+	{
+		tabNormale[iNorm] = 0;
+	}
+
+	for (int iFiN = 0; iFiN < NBCLIENTSNORMALE; iFiN++)
+	{
+		tabFileNormale[iFiN].DS = 0;
+		tabFileNormale[iFiN].estPrioritaire = false;
+	}
+
+	for (int iExpr = 0; iExpr < NBSTATION_EXPRESS; iExpr++)
+	{
+		tabExpress[iExpr] = 0;
+	}
+
+	for (int iFiE = 0; iFiE < CAPACITE_FILE_EXPRESS; iFiE++)
+	{
+		tabFileExpress[iFiE] = 0;
+	}
+}
+
+void placerClientFile(int nbArrivé, int *fileExpress, int *fileNormale, int *DS, int *tabNormale, ClientNormale *tabFileNormale, int *tabExpress, int *tabFileExpress)
+{
+	int i = 0;
+
+}
+
+int rand_a_b(int a, int b) 
+{
+	return rand() % (b - a) + a;
+}
+
+int GénérerNbArriv(void)
+{
+	double l, p, u;
+	l = exp(LAMBDA);
+	int k = 0;
+	p = 1;
+
+	do
+	{
+		k++;
+		u = rand_a_b(0, 1);
+		p *= u;
+	} while (p > l);
+
+	return k - 1;
 }
